3797e9fcbb2650a6cc87fe00394bd9267844ee81
Hanan Alashqar <116980013+iugcolud@users.noreply.github.com>
2023-05-19 23:36:46 +0200

    Add files via upload

---
+++ manual testing.php
@@ -0,0 +1,245 @@
+
+register
+
+<?php
+// Set the registration data
+$username = 'your_username';
+$email = 'your_email@example.com';
+$password = 'your_password';
+
+// Initialize cURL
+$ch = curl_init();
+
+// Set the cURL options
+curl_setopt($ch, CURLOPT_URL, 'https://www.example.com/register');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+    'username' => $username,
+    'email' => $email,
+    'password' => $password,
+    'confirm_password' => $password
+]));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Follow redirects if any
+
+// Execute the cURL request
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+// Close cURL
+curl_close($ch);
+
+// Verify the registration response
+if ($httpCode == 200 && strpos($response, 'Registration successful!') !== false) {
+    echo "Registration successful!";
+} else {
+    echo "Registration failed!";
+}
+?>
+
+
+
+
+
+contact
+
+<?php
+// Set the contact form data
+$name = 'John Doe';
+$email = 'john.doe@example.com';
+$message = 'This is a test message';
+
+// Initialize cURL
+$ch = curl_init();
+
+// Set the cURL options
+curl_setopt($ch, CURLOPT_URL, 'https://www.example.com/contact');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+    'name' => $name,
+    'email' => $email,
+    'message' => $message
+]));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Follow redirects if any
+
+// Execute the cURL request
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+// Close cURL
+curl_close($ch);
+
+// Verify the contact form submission response
+if ($httpCode == 200 && strpos($response, 'Thank you for your message!') !== false) {
+    echo "Contact form submitted successfully!";
+} else {
+    echo "Contact form submission failed!";
+}
+?>
+
+
+
+search
+
+<?php
+// Set the search query
+$searchQuery = 'example search query';
+
+// Initialize cURL
+$ch = curl_init();
+
+// Set the cURL options
+curl_setopt($ch, CURLOPT_URL, 'https://www.example.com/search');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+    'q' => $searchQuery
+]));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Follow redirects if any
+
+// Execute the cURL request
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+// Close cURL
+curl_close($ch);
+
+// Verify the search response
+if ($httpCode == 200 && strpos($response, 'Search results for') !== false) {
+    echo "Search successful!";
+} else {
+    echo "Search failed!";
+}
+?>
+
+
+
+add
+
+<?php
+// Retrieve the data from the form or request parameters
+$name = $_POST['name'];
+$description = $_POST['description'];
+$price = $_POST['price'];
+
+// Perform validation on the input data
+if (empty($name) || empty($description) || empty($price)) {
+    echo "Please fill in all the required fields.";
+} else {
+    // Perform the "add" operation, such as inserting the data into a database
+    // Example code to insert the data into a MySQL database
+    $connection = mysqli_connect('localhost', 'username', 'password', 'database_name');
+    
+    $sql = "INSERT INTO products (name, description, price) VALUES ('$name', '$description', '$price')";
+    $result = mysqli_query($connection, $sql);
+    
+    if ($result) {
+        echo "Data added successfully!";
+    } else {
+        echo "Failed to add data.";
+    }
+    
+    // Close the database connection
+    mysqli_close($connection);
+}
+?>
+
+
+
+
+delete
+
+<?php
+// Retrieve the ID of the record to delete
+$id = $_GET['id'];
+
+// Perform validation on the ID
+if (empty($id)) {
+    echo "Invalid ID provided.";
+} else {
+    // Perform the "delete" operation, such as removing the record from a database
+    // Example code to delete the record from a MySQL database
+    $connection = mysqli_connect('localhost', 'username', 'password', 'database_name');
+    
+    $sql = "DELETE FROM products WHERE id = $id";
+    $result = mysqli_query($connection, $sql);
+    
+    if ($result) {
+        echo "Record deleted successfully!";
+    } else {
+        echo "Failed to delete record.";
+    }
+    
+    // Close the database connection
+    mysqli_close($connection);
+}
+?>
+
+
+
+
+edit
+
+<?php
+// Retrieve the data from the form or request parameters
+$id = $_POST['id'];
+$name = $_POST['name'];
+$description = $_POST['description'];
+$price = $_POST['price'];
+
+// Perform validation on the input data
+if (empty($id) || empty($name) || empty($description) || empty($price)) {
+    echo "Please fill in all the required fields.";
+} else {
+    // Perform the "edit" operation, such as updating the data in a database
+    // Example code to update the data in a MySQL database
+    $connection = mysqli_connect('localhost', 'username', 'password', 'database_name');
+
+    $sql = "UPDATE products SET name='$name', description='$description', price='$price' WHERE id=$id";
+    $result = mysqli_query($connection, $sql);
+
+    if ($result) {
+        echo "Data updated successfully!";
+    } else {
+        echo "Failed to update data.";
+    }
+
+    // Close the database connection
+    mysqli_close($connection);
+}
+?>
+
+
+
+
+display
+
+
+<?php
+// Retrieve the data from a database
+$connection = mysqli_connect('localhost', 'username', 'password', 'database_name');
+
+$sql = "SELECT * FROM products";
+$result = mysqli_query($connection, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // Display the retrieved data
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "ID: " . $row['id'] . "<br>";
+        echo "Name: " . $row['name'] . "<br>";
+        echo "Description: " . $row['description'] . "<br>";
+        echo "Price: " . $row['price'] . "<br>";
+        echo "<br>";
+    }
+} else {
+    echo "No data found.";
+}
+
+// Close the database connection
+mysqli_close($connection);
+?>
+
+
+
+
